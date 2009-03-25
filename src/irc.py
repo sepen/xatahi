a96d55f772ab0bbcf742d75f8558bbe768e76475
Jose V Beneyto <sepen@crux.nu>
2009-03-25 16:05:09 +0000

    Modified python code to find a better way for the application's load. Temporal commit.
    git-svn-id: file:///tmp/xatahi-svnrepo/trunk@8 948cab4c-18d0-11de-9377-631aa55947fa

--- src/irc.py
+++ src/irc.py
@@ -1,39 +1,30 @@
 #!/usr/bin/env python
 
+
 import os
 import sys
 import socket
 import string
 
+
 class Irc:
 	
-	s, host, port, channel = None, None, None, None
+	s, rbuffer = None, None
+	host, port, channel = None, None, None
 	nick, ident, realname = None, None, None
 		
 	def __init__ (self, host, port, channel, nick):
 		self.host, self.port, self.channel = host, port, channel
 		self.nick, self.ident, self.realname = nick, nick, nick
 	
-	def connect(self):		
+	def connect(self):
 		readbuffer = ""
 		s = socket.socket( )
 		s.connect((self.host, self.port))
 		s.send("NICK %s\r\n" % self.nick)
 		s.send("USER %s %s * :%s\r\n" % (self.ident, self.host, self.realname))
-		s.send("JOIN %s\r\n" % self.channel)
 		readbuffer = s.recv(1024)
-		temp = string.split(readbuffer, "\n")
-		readbuffer = temp.pop( )
-		for line in temp:
-			out = ""
-			line = string.rstrip(line)
-			vect = string.split(line)
-			# server ping pong
-			if (vect[0] == "PING"):
-				s.send("PONG %s\r\n" % vect[1])
-			else:
-				out = out + line
-		self.s = s
-		return out
+		self.rbuffer = readbuffer
+		return
 
 # End of file
--- src/xatahi.py
+++ src/xatahi.py
@@ -10,18 +10,37 @@ import irc
 
  
 class Xatahi:
-	
+
+	wTree, irc, textview, textentry = None, None, None, None
+
 	def __init__(self):
-		self.glade = gtk.glade.XML("xatahi.glade")
-		self.glade.signal_autoconnect(self)
-		self.glade.get_widget("window1").show_all()
-		
+		self.wTree = gtk.glade.XML("xatahi.glade")
+		self.wTree.signal_autoconnect(self)
+		self.wTree.get_widget("window1").show_all()
+		self.textview = self.wTree.get_widget("textview1")
+		self.textbuffer = self.textview.get_buffer()
+		self.textentry = self.wTree.get_widget("entry1")
+		self.show_help()
+		self.show_irc_connection()
+
+	def show_help(self):
+		infile = open("help.txt", "r")
+		if infile:
+			string = infile.read()
+			infile.close()
+			self.textbuffer.set_text(string)
+
+	def show_irc_connection(self):
+		self.textentry.set_text("Type: /server servername.domain")
+		#self.irc = irc.Irc("mikeux.dyndns.org", 6667, "#mikeux", "xatahi")
+		#self.irc.connect()
+		#string = self.irc.rbuffer
+		#textbuffer.set_text(string)
+
 	def on_window1_delete_event(self, widget, event):
 		gtk.main_quit()
 
 
 if __name__ == "__main__":
 	x = Xatahi()
-	i = irc.Irc("mikeux.dyndns.org", 6667, "#mikeux", "xatahi")
-	i.connect()
 	gtk.main()
