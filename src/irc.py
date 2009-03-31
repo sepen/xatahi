2602d6a05c8de54991c77ac75668c9244a99d02b
Jose V Beneyto <sepen@crux.nu>
2009-03-31 09:16:29 +0000

    Modified for abstractions and implemented more features. Still in premature status
    git-svn-id: file:///tmp/xatahi-svnrepo/trunk@11 948cab4c-18d0-11de-9377-631aa55947fa

--- src/irc.py
+++ src/irc.py
@@ -8,27 +8,50 @@ import string
 
 
 class Irc:
-	
-	s, rbuffer = None, None
+
+	s, rbuffer, status = None, None, None
 	host, port, channel = None, None, None
 	nick, ident, realname = None, None, None
-		
+
 	def __init__ (self, host, port, channel, nick):
-		self.s = socket.socket()
 		self.rbuffer = ""
+		self.status = "disconnected"
 		self.host, self.port, self.channel = host, port, channel
 		self.nick, self.ident, self.realname = nick, nick, nick
-	
+		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	def close(self):
+		self.s.close()
+		self.status = "disconnected"
+
 	def read(self):
-		self.rbuffer = self.s.recv(1024)	
-	
+		if self.status == "connected" or "joined":
+			self.rbuffer = self.s.recv(1024)
+			if self.rbuffer != None:
+				fields = self.rbuffer.split()
+				if fields[0] == "PING":
+					self.s.send("PONG " + fields[1])
+
+	def send(self, string):
+		if self.status == "connected" or "joined":
+			self.s.send("%s\r\n", string)
+
 	def connect(self):
+		self.status = "disconnected"
 		self.s.connect((self.host, self.port))
 		self.s.send("NICK %s\r\n" % self.nick)
 		self.s.send("USER %s %s * :%s\r\n" % (self.ident, self.host, self.realname))
-		
+		self.status = "connected"
+		self.read()
+
 	def join(self):
 		self.s.send("JOIN %s\r\n" % self.channel)
-		self.s.send("PRIVMSG %s :%s\r\n" % (self.channel, "lokooooooo"))
+		self.send_to_channel("lokooooooo")
+		self.status = "joined"
+		self.read()
+
+	def send_to_channel(self, string):
+		if self.status == "connected" or self.status == "joined":
+			self.s.send("PRIVMSG %s :%s\r\n" % (self.channel, string))
 
 # End of file
--- src/xatahi.py
+++ src/xatahi.py
@@ -11,7 +11,8 @@ import irc
  
 class Xatahi:
 
-	wTree, irc, textentry, textentrycontents = None, None, None, None
+	wTree, irc, t = None, None, None
+	textentry, textentrycontents = None, None
 	textview, textbuffer, textviewcontents = None, None, None
 
 	def __init__(self):
@@ -23,10 +24,7 @@ class Xatahi:
 		self.textbuffer = self.textview.get_buffer()
 		self.textentrycontents = ""
 		self.textviewcontents = ""
-		self.irc = irc.Irc("mikeux.dyndns.org", 6667, "#mikeux", "xatahi")
-		#self.show_help()
-		#self.irc_connect()
-		#self.irc_join()
+		self.irc = irc.Irc("mikeux.dyndns.org", 6667, "#test", "xatahi")
 
 	def show_help(self):
 		infile = open("help.txt", "r")
@@ -35,28 +33,50 @@ class Xatahi:
 			infile.close()
 			self.append_to_textview(string)
 
+	def on_window1_delete_event(self, widget, event):
+		self.irc.close()
+		gtk.main_quit()
+
 	def append_to_textview(self, line):
 		self.textviewcontents = self.textviewcontents + line
 		self.textbuffer.set_text(self.textviewcontents)
 
-	def on_entry1_enter_pressed(self, widget):
+	def on_entry1_enter_pressed(self, widget):		
 		self.textentrycontents = widget.get_text()
-		widget.set_text("")
-		self.append_to_textview(">%s< %s\n" % (self.irc.nick, self.textentrycontents))
+		if self.textentrycontents != None:
+			widget.set_text("")
+			self.do_tasks(self.textentrycontents)
 
-	def irc_connect(self):
+	def do_tasks(self, string):
+		if string == "/h" or string == "/help":
+			self.show_help()
+		elif string == "/s" or string == "/server":
+			if self.irc.status == "disconnected":
+				self.connect_to_server()
+			else:
+				self.append_to_textview("**** You are connected to a server\n")
+		elif string == "/q" or string == "/quit":
+			self.irc.close()
+			gtk.main_quit()
+		elif string == "/j" or string == "/join":
+			if self.irc.status == "connected" or self.irc.status == "joined":
+				self.join_to_channel()
+			else:
+				self.append_to_textview("**** You must connect to a server\n")
+		else:
+			if self.irc.status == "connected" or self.irc.status == "joined":
+				self.append_to_textview(">%s< %s\n" % (self.irc.nick, string))
+				self.irc.send_to_channel(string)
+
+	def connect_to_server(self):
+		self.append_to_textview("\n\n===> Connecting to %s at port %s...\n" % (self.irc.host, self.irc.port))
 		self.irc.connect()
-		self.irc.read()
-		self.append_to_textview("\n\nConnecting to %s at port %s...\n" % (self.irc.host, self.irc.port))
-
-	def irc_join(self):
-		self.irc.join()
-		self.irc.read()
 		self.append_to_textview(self.irc.rbuffer)
-		self.append_to_textview("Now talking on: %s\n" % self.irc.channel)
 
-	def on_window1_delete_event(self, widget, event):
-		gtk.main_quit()
+	def join_to_channel(self):
+		self.irc.join()
+		self.append_to_textview(self.irc.rbuffer)
+		self.append_to_textview("===> Now talking on: %s\n" % self.irc.channel)
 
 
 if __name__ == "__main__":
